e025edae65964f0bc46a944eba44591a7e29c4eb
AzureAD\AdityaKumarDubey <adityadubey20002001@gmail.com>
2024-10-23 14:39:43 +0530

    make register page and make internal server page and modify login page

--- src/app/views/view/internal-server-error/internal-server-error.component.ts
+++ src/app/views/view/internal-server-error/internal-server-error.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Renderer2, ViewChild} from '@angular/core';
+
+declare const $: any;
 
 @Component({
   selector: 'app-internal-server-error',
@@ -7,6 +9,20 @@ import { Component } from '@angular/core';
   templateUrl: './internal-server-error.component.html',
   styleUrl: './internal-server-error.component.scss'
 })
-export class InternalServerErrorComponent {
+export class InternalServerErrorComponent implements AfterViewInit {
+
+
+  @ViewChild('loadingDiv') loadingDiv!: ElementRef;
+
+  constructor(private renderer: Renderer2) {
+ 
+  }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.renderer.removeClass(this.loadingDiv.nativeElement, 'loading');
+    }, 1000);
+  }
+
 
 }
